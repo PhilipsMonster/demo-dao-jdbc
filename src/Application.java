f1709272891a192311925d310dbfa9d9ba006019
PhilipsMonster <philipsmonster@gmail.com>
2020-12-12 20:00:40 -0300

    SellerDaoJDBC and DaoFactory

--- src/Application.java
+++ src/Application.java
@@ -1,3 +1,5 @@
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,6 +11,8 @@ public class Application {
         Department obj = new Department(1, "Books");
 
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(seller);
     }

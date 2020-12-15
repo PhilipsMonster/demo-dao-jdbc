e067647d95a6157658f20075556753137fdb798c
PhilipsMonster <philipsmonster@gmail.com>
2020-12-15 08:52:55 -0300

    Reusing instantiation

--- src/Application.java
+++ src/Application.java
@@ -10,8 +10,8 @@ public class Application {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        System.out.println("=== TEST 1: seller findById ===");
         Seller seller = sellerDao.findById(3);
-
         System.out.println(seller);
     }
 }
